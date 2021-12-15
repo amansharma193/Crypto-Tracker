132f54c68652a5f26606892af18ae6f8f6a90bcc
Aman Sharma <amanantonio1932815@gmail.com>
2021-12-16 01:50:50 +0530

    project completed before deployment

--- src/Random.js
+++
@@ -1,9 +0,0 @@
-const Random=(max)=>{
-  let randomArray=[];
-  while(randomArray.length<max){
-    let idx=Math.floor(Math.random() * max);
-    if(randomArray.indexOf(idx)==-1) randomArray.push(idx);
-  }
-  return randomArray;
-}
-export default Random;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Home from "./Home";
+import App from './App';
 ReactDOM.render(
-  <Home/>,
+  <App />,
   document.getElementById('root')
 );
